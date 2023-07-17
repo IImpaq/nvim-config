20f60a456d4ce62d7780394174264b4d9b68f156
Marcus Gugacs <iimpaq@proton.me>
2023-07-17 16:30:07 +0200

    Fixed formatting

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,10 +6,8 @@ return {
 * Description: These plugins don't need any particular          *
 *              configuration. That's why they are just included *
 *              here without any particular settings.            *
-*                                                               *
 *****************************************************************
 ]]
   "tpope/vim-fugitive",
 	"tpope/vim-rhubarb",
-
 }
