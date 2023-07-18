d2e688b2426140ae8b91858bab1ebd291ee3c61e
Marcus Gugacs <iimpaq@proton.me>
2023-07-18 11:22:02 +0200

    Added lualine, treesitter and telescope

---
+++ lua/options/init.lua
@@ -0,0 +1,9 @@
+-- General options
+require("options.vim")
+require("options.theme")
+require("options.keymap")
+
+-- Plguin options
+require("options.lualine")
+require("options.telescope")
+require("options.treesitter")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,16 +1,51 @@
 return {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
 --[[
 *****************************************************************
 *                         GIT PLUGINS                           *
 *****************************************************************
-* Description: These plugins don't need any particular          *
-*              configuration. That's why they are just included *
-*              here without any particular settings.            *
-*****************************************************************
 ]]
+  -- Git client
   "tpope/vim-fugitive",
+  -- GitHub extension to fugitive
   "tpope/vim-rhubarb",
 
+--[[
+*****************************************************************
+*                       VISUAL PLUGINS                          *
+*****************************************************************
+]]
+  -- Status line at the bottom of the screen
+  'nvim-lualine/lualine.nvim',
+
+--[[
+*****************************************************************
+*                  FILE MANAGEMENT PLUGINS                      *
+*****************************************************************
+]]
+  {
+    'nvim-telescope/telescope.nvim', branch = '0.1.x',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+  },
+--[[
+*****************************************************************
+*                    CODE COMPLETION PLUGINS                    *
+*****************************************************************
+* Description: Here is everything coding related. This section  *
+*              will make nvim feel like a real IDE. But faster  *
+*****************************************************************
+]]
+  {
+    -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {},
+    build = ':TSUpdate',
+  },
 
 --[[
 *****************************************************************
@@ -21,5 +56,6 @@ return {
 *              here without any particular settings.            *
 *****************************************************************
 ]]
+  -- Automatic spacing detection
   "tpope/vim-sleuth",
 }
