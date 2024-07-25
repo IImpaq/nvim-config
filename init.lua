b3d534dd508737f5f7ec816508557c6818d7bcee
Marcus Gugacs <iimpaq@proton.me>
2024-07-25 14:14:35 +0200

    Configuration cleanup

--- init.lua
+++ init.lua
@@ -1,28 +1,5 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
---[[
-*****************************************************************
-*                      LAZY PLUGIN MANAGER                      *
-*****************************************************************
-* Description: Including lazy plugin manager. Individual        *
-*              plugins are included in the lua/plugins/ folder  *
-*                                                               *
-*****************************************************************
-]]
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
+require("config.lazy")
 
-require("lazy").setup("plugins")
-
-require("options.init")
--- lua/options/glow.lua
+++
@@ -1,9 +0,0 @@
-require("glow").setup({
-  border = "shadow", -- floating window border config
-  pager = false,
-  -- width = 80,
-  -- height = 100,
-  width_ratio = 0.5, -- maximum width of the Glow window compared to the nvim window size (overrides `width`)
-  height_ratio = 0.7,
-})
-
