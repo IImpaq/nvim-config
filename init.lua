247abd51c849371b9e7bdf36eaed3df2b8dde24d
Marcus Gugacs <iimpaq@proton.me>
2024-07-26 14:26:03 +0200

    Added more structure to plugins and configuration

--- init.lua
+++ init.lua
@@ -1,7 +1,4 @@
-require("config.vim")
+require("config.keymap")
 require("config.lazy")
-
-require("config.theme")
-
-require("config.gitsigns")
+require("config.vim")
 
--- lua/config/vim.lua
+++ lua/config/vim.lua
@@ -1,6 +1,59 @@
--- Make sure to setup `mapleader` and `maplocalleader` before
--- loading lazy.nvim so that mappings are correct.
--- This is also a good place to setup other settings (vim.opt)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- VIM SETTINGS
+local o = vim.o
+local wo = vim.wo
+local cmd = vim.cmd
+
+-- Set highlight on search
+o.hlsearch = false
+
+-- Make line numbers default
+wo.number = true
+
+-- Enable relative line numbers
+wo.relativenumber = true
+
+-- Enable mouse mode
+o.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+o.clipboard = 'unnamedplus'
+
+-- Enable break indent
+o.breakindent = true
+
+-- Save undo history
+o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+o.ignorecase = true
+o.smartcase = true
+
+-- Tab configuration
+--  Expand tab input with space characters
+o.expandtab = true
+--  syntax aware indentations for newline inserts
+o.smartindent = true
+--  Number of space characters per tab
+o.tabstop = 2
+--  Spaces per indentation level
+o.shiftwidth = 2
+
+-- Keep signcolumn on by default
+wo.signcolumn = 'yes'
+
+-- Decrease update time
+o.updatetime = 250
+o.timeout = true
+o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+o.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+o.termguicolors = true
+
+-- Set colorscheme
+cmd.colorscheme "catppuccin"
 
